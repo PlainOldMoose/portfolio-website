0103260b29c23cbd795c9c212e9642db74fcac32
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 18:15:29 +0000

    Implemented basic Hero greeting

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,9 +1,17 @@
 export default function Hero() {
-
+    const ASCII_LOGO = ` ██╗     ██╗   ██╗ ██████╗ ██╗   ██╗███████╗
+ ██║     ██║   ██║██╔═══██╗██║   ██║██╔════╝
+ ██║     ██║   ██║██║   ██║██║   ██║█████╗
+ ██║     ██║   ██║██║▄▄ ██║██║   ██║██╔══╝
+ ███████╗╚██████╔╝╚██████╔╝╚██████╔╝███████╗
+ ╚══════╝ ╚═════╝  ╚══▀▀═╝  ╚═════╝ ╚══════╝`;
     return (
         <section
-            id = "home">
-            <div>Hello, I'm Luque</div>
+            id="home">
+            <div className="border-2 border-[#00ff41] bg-black text-center max-w-4xl mx-auto">
+                <div className="text-terminal-green">Hello, I'm</div>
+                <pre className="text-terminal-green">{ASCII_LOGO}</pre>
+            </div>
         </section>
     );
 }
