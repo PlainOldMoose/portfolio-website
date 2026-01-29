b6663165d0af2a9a1e0a41a8266874b45bd2bf46
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 19:38:57 +0000

    Minor typo

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
         <section id="about" className="py-20">
             <div className="flex items-center gap-3 mb-4">
                 <div className="h-px flex-1 bg-linear-to-r from-terminal-green/50"/>
-                <h2 className="text-terminal-green">{'<CONACT_ME>'}</h2>
+                <h2 className="text-terminal-green">{'<CONTACT_ME>'}</h2>
                 <div className="h-px flex-1 bg-linear-to-l from-terminal-green/50"/>
             </div>
         </section>
