461b1bd71d1ac762a0243e49e890cb2b49a76202
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 20:26:32 +0000

    Removed redundant classname font setting

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { Terminal } from "lucide-react";
 export default function Navbar() {
 
     return (
-        <nav className="flex items-center justify-between p-4 border-b border-[#00ff41] text-terminal-green font-terminal">
+        <nav className="flex items-center justify-between p-4 border-b border-[#00ff41] text-terminal-green">
             <div className="flex items-center gap-2">
                 <Terminal size={16}/>
                 <div className="text-2xl font-bold">LUQUE.dev</div>
