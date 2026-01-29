40cc505176171d08c6af25a3e0425a994aba838f
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 19:37:29 +0000

    Implemented contact section skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/layout/Navbar.tsx";
 import Hero from "./components/sections/Hero.tsx";
 import About from "./components/sections/About.tsx";
 import Projects from "./components/sections/Projects.tsx";
+import Contact from "./components/sections/Contact.tsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
               <Hero/>
               <About/>
               <Projects/>
+              <Contact/>
           </main>
       </div>
   );
---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <section id="about" className="py-20">
+            <div className="flex items-center gap-3 mb-4">
+                <div className="h-px flex-1 bg-linear-to-r from-terminal-green/50"/>
+                <h2 className="text-terminal-green">{'<CONACT_ME>'}</h2>
+                <div className="h-px flex-1 bg-linear-to-l from-terminal-green/50"/>
+            </div>
+        </section>
+    );
+}
