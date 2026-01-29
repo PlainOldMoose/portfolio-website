515d58fc11273f3094fad5823920c62905b0a04c
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 19:57:54 +0000

    Imported lucide and added comments to structure of hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,3 +1,5 @@
+import {ChevronRight} from "lucide-react";
+
 export default function Hero() {
     const ASCII_LOGO = ` ██╗     ██╗   ██╗ ██████╗ ██╗   ██╗███████╗
  ██║     ██║   ██║██╔═══██╗██║   ██║██╔════╝
@@ -7,8 +9,11 @@ export default function Hero() {
  ╚══════╝ ╚═════╝  ╚══▀▀═╝  ╚═════╝ ╚══════╝`;
     return (
         <section
-            id="home" className="py-2">
+            id="home" className="py-10">
+            {/*Hero Window*/}
             <div className="border-2 border-[#00ff41] bg-black text-center max-w-4xl mx-auto">
+
+                {/* Window Header*/}
                 <div className="px-4 py-2 border-b border-[#00ff41]/50 bg-[#00ff41]/5">
                     <div className="flex gap-2">
                         <div className="w-3 h-3 rounded-full bg-[#ff3333]"/>
@@ -16,8 +21,18 @@ export default function Hero() {
                         <div className="w-3 h-3 rounded-full bg-[#00ff41]"/>
                     </div>
                 </div>
-                <div className="text-terminal-green py-2">Hello, I'm</div>
-                <pre className="text-terminal-green py-2">{ASCII_LOGO}</pre>
+
+                {/*Window Body*/}
+                <div className="px-15 py-15 text-terminal-green">
+                    <span>Hello, I'm</span>
+                    <pre className="py-10">{ASCII_LOGO}</pre>
+                    <div className="flex justify-center items-center gap-2">
+                        <ChevronRight size={16}/>
+                        <span>
+                        JUNIOR SOFTWARE ENGINEER
+                        </span>
+                    </div>
+                </div>
             </div>
         </section>
     );
