169069e3796e8848175522a3719579ad48a3cef2
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 21:57:40 +0000

    Added z-50 to nav class to stop conflicts with other elements intended to be behind the navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { Terminal } from "lucide-react";
 export default function Navbar() {
 
     return (
-        <nav className="sticky top-0 flex items-center justify-between p-4 border-b border-terminal-green text-terminal-green bg-black">
+        <nav className="sticky top-0 z-50 flex items-center justify-between p-4 border-b border-terminal-green text-terminal-green bg-black">
             <div className="flex items-center gap-2">
                 <Terminal size={30}/>
                 <div className="text-2xl font-bold pb-1">guest@luque.dev</div>
