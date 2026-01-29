8bfd30772b29280f14fbd645ef053a165a2a5659
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 18:40:29 +0000

    Added bottom border to navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
 
     return (
-        <nav className="flex items-center justify-between p-4 bg-black">
+        <nav className="flex items-center justify-between p-4 bg-black border-b border-[#00ff41]">
             <div className="text-xl font-bold text-terminal-green">LUQUE.dev</div>
             <div className="flex gap-6 text-terminal-green">
                 <a>About</a>
