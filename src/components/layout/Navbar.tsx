5da341d98f9d96775d155fa9564fc5eddf8cb17b
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 20:25:57 +0000

    Implemented VT323 to navbar and rest of site

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,15 @@
+import { Terminal } from "lucide-react";
+
 export default function Navbar() {
 
     return (
-        <nav className="flex items-center justify-between p-4 bg-black border-b border-[#00ff41]">
-            <div className="text-xl font-bold text-terminal-green">LUQUE.dev</div>
-            <div className="flex gap-6 text-terminal-green">
+        <nav className="flex items-center justify-between p-4 border-b border-[#00ff41] text-terminal-green font-terminal">
+            <div className="flex items-center gap-2">
+                <Terminal size={16}/>
+                <div className="text-2xl font-bold">LUQUE.dev</div>
+                <span className="cursor-blink">_</span>
+            </div>
+            <div className="flex gap-6">
                 <a>About</a>
                 <a>My Projects</a>
                 <a>Contact</a>
