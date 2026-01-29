b4348b586547eec673dbc92e04105f987243378d
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 19:24:56 +0000

    Implemented projects section skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/layout/Navbar.tsx";
 import Hero from "./components/sections/Hero.tsx";
 import About from "./components/sections/About.tsx";
+import Projects from "./components/sections/Projects.tsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <main>
               <Hero/>
               <About/>
+              <Projects/>
           </main>
       </div>
   );
---
+++ src/components/sections/Projects.tsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+    return (
+        <section id="projects" className="py-20">
+            <div className="flex items-center gap-3 mb-4">
+                <div className="h-px flex-1 bg-linear-to-r from-terminal-green/50"/>
+                <h2 className="text-terminal-green">{'<MY_PROJECTS>'}</h2>
+                <div className="h-px flex-1 bg-linear-to-l from-terminal-green/50"/>
+            </div>
+        </section>
+    );
+}
