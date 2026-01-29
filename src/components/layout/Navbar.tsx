2106277118d1f5763060c14f0b72274cabb0eb78
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-29 21:09:20 +0000

    Updated variable name

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { Terminal } from "lucide-react";
 export default function Navbar() {
 
     return (
-        <nav className="flex items-center justify-between p-4 border-b border-[#00ff41] text-terminal-green">
+        <nav className="flex items-center justify-between p-4 border-b border-terminal-green text-terminal-green">
             <div className="flex items-center gap-2">
                 <Terminal size={30}/>
                 <div className="text-2xl font-bold pb-1">guest@luque.dev</div>
