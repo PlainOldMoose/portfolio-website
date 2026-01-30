4acddf0788140cb64ed86749301b9b02e2aba5ea
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-30 01:40:47 +0000

    Centred nav bar buttons and minor refactor to style

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,9 +5,10 @@ export default function Navbar() {
     const [activeSection, setActiveSection] = useState("home");
 
     const navLinks = [
-        {id: "about", label: "ABOUT"},
-        {id: "projects", label: "MY_PROJECTS"},
-        {id: "contact", label: "CONTACT"},
+        {id: "home", label: "HOME", key: "F1"},
+        {id: "about", label: "ABOUT", key: "F2"},
+        {id: "projects", label: "MY_PROJECTS", key: "F3"},
+        {id: "contact", label: "CONTACT", key: "F4"},
     ];
 
     useEffect(() => {
@@ -31,7 +32,7 @@ export default function Navbar() {
 
     return (
         <nav
-            className="sticky top-0 z-50 flex items-center justify-between border-b border-terminal-green text-terminal-green bg-black">
+            className="sticky top-0 z-50 grid grid-cols-3 items-center border-b border-terminal-green text-terminal-green bg-black">
             <div className="flex items-center gap-2">
                 <Terminal size={30}/>
                 <a href="#" className="text-2xl font-bold pb-1">guest@luque.dev</a>
@@ -39,14 +40,14 @@ export default function Navbar() {
             </div>
 
             {/*Nav bar links inserted here*/}
-            <div className="flex p-2 gap-6">
-                {navLinks.map(({id, label}) => (
+            <div className="flex justify-center p-2 ">
+                {navLinks.map(({id, label, key}) => (
                     <a key={id}
                        href={`#${id}`}
                        className={`px-4 py-2 ${
-                           activeSection === id ? "bg-terminal-green text-black" : "hover:bg-terminal-green/20"
+                           activeSection === id ? "bg-terminal-green text-black" : "hover:bg-terminal-green/10"
                        }`}
-                    >{label}</a>
+                    ><span className="text-xs mr-1 opacity-50">[{key}]</span>{label}</a>
                 ))}
             </div>
         </nav>
