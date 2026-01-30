766a9d4d8c2cae0f2893e099cc805544d50a3273
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-30 00:50:02 +0000

    Implemented scroll tracking for highlighting navbar selection

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,18 +1,53 @@
-import { Terminal } from "lucide-react";
+import {Terminal} from "lucide-react";
+import {useEffect, useState} from "react";
 
 export default function Navbar() {
+    const [activeSection, setActiveSection] = useState("home");
+
+    const navLinks = [
+        {id: "about", label: "ABOUT"},
+        {id: "projects", label: "MY_PROJECTS"},
+        {id: "contact", label: "CONTACT"},
+    ];
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = ["home", "about", "projects", "contact"];
+
+            for (const section of sections) {
+                const element = document.getElementById(section);
+                if (element) {
+                    const rect = element.getBoundingClientRect();
+                    if (rect.top <= 100 && rect.bottom >= 100) {
+                        setActiveSection(section);
+                        break;
+                    }
+                }
+            }
+        }
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
-        <nav className="sticky top-0 z-50 flex items-center justify-between p-4 border-b border-terminal-green text-terminal-green bg-black">
+        <nav
+            className="sticky top-0 z-50 flex items-center justify-between border-b border-terminal-green text-terminal-green bg-black">
             <div className="flex items-center gap-2">
                 <Terminal size={30}/>
                 <a href="#" className="text-2xl font-bold pb-1">guest@luque.dev</a>
                 <span className="cursor-blink">_</span>
             </div>
-            <div className="flex gap-6">
-                <a href="#about">About</a>
-                <a href="#projects">My Projects</a>
-                <a href="#contact">Contact</a>
+
+            {/*Nav bar links inserted here*/}
+            <div className="flex p-2 gap-6">
+                {navLinks.map(({id, label}) => (
+                    <a key={id}
+                       href={`#${id}`}
+                       className={`px-4 py-2 ${
+                           activeSection === id ? "bg-terminal-green text-black" : "hover:bg-terminal-green/20"
+                       }`}
+                    >{label}</a>
+                ))}
             </div>
         </nav>
     );
